981016c97aeeb7748a63990fe4e67b709434a6e1
Wooly <mirajkharel2020@gmail.com>
2025-08-31 16:51:57 +0545

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,9 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'templates')));
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'templates', 'index.html'));
+app.get('/connect', (req, res) => {
+    const b64data = req.query.b64;
+    if (b64data) {
+        console.log('Received data:', b64data); // logs in Render
+        res.send('Data received successfully ✅');
+    } else {
+        res.status(400).send('No data received');
+    }
 });
-
-app.post('/connect', (req, res) => {
-    const data = req.body;
-    console.log('Received data:', data); // You’ll see this in Render logs
-    res.send('Data received!');
-});
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
